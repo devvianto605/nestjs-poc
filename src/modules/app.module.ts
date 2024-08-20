1055a4b934aa755c5ce6fa1ee90a5020b63e091b
Kor Korawit <root@DESKTOP-9V94FI3.>
2024-08-20 13:02:08 +0700

    feature: delete default api prototype imports

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { CommonModule } from './common';
 import { ConfigurationModule } from './configuration/configuration.module';
-import { PassengerModule } from './passenger/passenger.module';
 
 @Module({
     imports: [
         CommonModule,
-        PassengerModule,
         ConfigurationModule
     ]
 })
